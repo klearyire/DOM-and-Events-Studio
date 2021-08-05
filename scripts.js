4d0abd0c4662f494b2f6d81dd695c5a53acf82ea
Kiaran <klearyire@gmail.com>
2021-08-05 19:42:27 -0400

    takeoff

--- scripts.js
+++ scripts.js
@@ -1,2 +1,16 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+// parseInt() - function for looking at what's in an elementById
+
+window.addEventListener("load", init);
+
+function init() {
+    const takeOff = document.getElementById("takeoff");
+    takeOff.addEventListener("click", function() {
+        const flightStatus = this.getElementsById("Flight status");
+        const reply = window.confirm("Confirm that the shuttle is ready for takeoff.")
+            if (answer === true) {
+                flightStatus = "Shuttle in flight."
+            }
+    });
+}
