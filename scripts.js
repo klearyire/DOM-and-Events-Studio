006dac042138d09e7038c6341e2c65d1da27c55a
Kiaran <klearyire@gmail.com>
2021-08-05 20:07:12 -0400

    new

--- scripts.js
+++ scripts.js
@@ -6,11 +6,17 @@ window.addEventListener("load", init);
 
 function init() {
     const takeOff = document.getElementById("takeoff");
-    const flightStatus = document.getElementById("flightStatus");
     takeOff.addEventListener("click", function() {
         const reply = window.confirm("Confirm that the shuttle is ready for takeoff.")
-            if (answer === true) {
-                flightStatus.innerText = "Shuttle in flight."
+            if (reply === true) {
+                document.getElementById("flightStatus") = "Shuttle in flight."
+                document.getElementById("shuttleBackground").style.background = "blue"
+                
+
             }
+    const landing = document.getElementById("landing");
+    landing.addEventListener("click", function() {
+
+    })
     });
 }
